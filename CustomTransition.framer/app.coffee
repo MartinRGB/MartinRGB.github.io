36dc0a9d4e0e237c7832c0d77dce5a26bc7ee2bf
Martin <qiuyinsen@gmail.com>
2016-05-01 23:40:34 +0800

    update new Prototype
    update new Prototype

---
+++ CustomTransition.framer/app.coffee
@@ -0,0 +1,421 @@
+# Import file "CustomTransition"
+sketch = Framer.Importer.load("imported/CustomTransition@1x")
+Rebirth = ->
+	# Import file "CustomTransition" (sizes and positions are scaled 1:2)
+	sketch = Framer.Importer.load("imported/CustomTransition@2x")
+	# Make 'sketch.' as global variable
+	Utils.globalLayers(sketch)
+
+	# Setting Default Animations Properties
+	Framer.Defaults.Animation =
+		curve : 'spring(180, 20, 0)'
+		time: .4
+
+	BlackMask.opacity = 0;
+
+	# Init the Layer
+	cardOne = new Layer
+		  x: 30, y: 724, width: 240, height: 340,borderRadius:10,backgroundColor: "#FFF",borderWidth: 1,borderColor: "#C7C7C7"
+	newHitArea = new Layer
+		  x: 0, y: 0, width: 240, height: 340,backgroundColor: "#000",opacity: 0,originX: 0,originY: 0
+	EditHitArea = new Layer
+		  x: 520, y: 0, width: 120, height: 120,backgroundColor: "#000",opacity: 0,originX: 0,originY: 0
+	newGroup = new Layer
+		x:0,y:0,width: 750,height: 1334,backgroundColor: "clear"
+		
+	newGroup.addSubLayer(cardOne)
+	cardOne.addSubLayer(newScrollView)
+	cardOne.addSubLayer(navBar)
+	cardOne.addSubLayer(statusBar)
+	
+	cardOne.addSubLayer(nowDate)
+	cardOne.addSubLayer(Title)
+	cardOne.addSubLayer(newHitArea)
+	newGroup.addSubLayer(EditHitArea)
+	BG1.addSubLayer(Card2)
+	cardOne.clip = true
+
+	keyBoard.visible = false
+	EditBG2.visible = false
+	keyBoard.y = 1334
+	cursor.opacity = 0
+
+	PublishView.originX = 0
+	PublishView.originY = 0
+	PublishView.opacity = 1
+	PublishView.scale = 0.32
+	PublishView.y = 427
+	cardOne.addSubLayer(PublishView)
+	PublishView.placeBehind(navBar)
+	BlackMask2.originX = 0
+	BlackMask2.originY = 0
+	BlackMask2.opacity = 0
+	BlackMask2.scale = 0.32
+	cardOne.addSubLayer(BlackMask2)
+	BlackMask2.placeBehind(PublishView)
+	Publish2.opacity = 0
+	Back2.opacity = 0
+
+	step = 1
+
+	Mask_for_Text.visible = true
+	Mask_for_Text.opacity = 0
+	ShiningLayer.addSubLayer(ShingingShape)
+	ShiningLayer.clip = true
+	ShingingShape.x = -100
+	Preview.opacity = 0
+
+	Title.x = 16
+	Title.y = 216
+	nowDate.x = 16
+	nowDate.y = 290
+
+	scroll = ScrollComponent.wrap(sketch.newScrollView)
+	scroll.scrollHorizontal = false
+	scroll.scrollVertical = false
+
+	newScrollView.originX = 0
+	newScrollView.originY = 0
+	newScrollView.scale  = 0.32
+	newScrollView.y  = -60
+	navBar.originX = 0
+	navBar.originY = 0
+	navBar.scale = 0.32
+	statusBar.y = -20
+	navBar.y = -42
+	statusBar.originX = 0
+	statusBar.originY = 0
+	statusBar.scale = 0.32
+	BG2.opacity = 0
+
+	newCard.y = 770
+	newCard.opacity = 0
+	newCard.scale = 0.8
+
+	# Init the Function
+	openTheCard = ->
+		cardOne.animate
+			properties: 
+				scale: 3.125
+				x:255
+				y:460
+				height:435
+				borderRadius: 0
+				borderWidth: 0
+		Title.opacity = 0
+		nowDate.opacity = 0
+		statusBar.animate
+			properties:
+				y:0
+				opacity:1
+		navBar.animate
+			properties:
+				y:0
+				opacity:1
+					
+		newHitArea.scale = 0.12
+		newHitArea.width = 480
+			
+		Mask_for_Text.animate
+			properties:
+				opacity:1
+			
+		ShingingShape.animate
+			properties:
+				x:500
+			repeat:3	
+			
+		Utils.delay 3.6, ->
+			Mask_for_Text.opacity = 0
+			BG2.opacity = 1
+			scroll.scrollVertical = true	
+			cardOne.backgroundColor = "clear"
+				
+		newScrollView.animate
+			properties:
+				y:0
+		BG1.animate
+			properties:
+				scale:0.8
+		BlackMask.animate
+			properties:
+				opacity:0.5
+		
+		newGroup.y = 0
+		
+
+	closeTheCard = ->
+		previewMode()
+		cardOne.animate
+			properties: 
+				scale: 1
+				x:30
+				y:724
+				height:340
+				borderRadius: 10
+				borderWidth: 1
+		Utils.delay 0.2, ->
+			Title.opacity = 1
+			nowDate.opacity = 1
+		statusBar.animate
+			properties:
+				y:-20
+				opacity:0
+		navBar.animate
+			properties:
+				y:-42
+				opacity:0
+		newGroup.animate
+			properties: 
+				y:0
+				
+		newHitArea.scale = 1
+		newHitArea.width = 240
+		
+		BG2.animate
+			properties:
+				opacity:0
+		Mask_for_Text.animate
+			properties:
+				opacity:0
+		ShingingShape.x = -100
+				
+		scroll.scrollVertical = false
+		newScrollView.animate
+			properties:
+				y:-60
+		BG1.animate
+			properties:
+				scale:1
+		BlackMask.animate
+			properties:
+				opacity:0
+
+	editMode = ->
+		keyBoard.visible = true
+		EditBG2.visible = true
+		keyBoard.y = 1334
+		
+		Edit.opacity = 0
+		Preview.opacity = 1
+		keyBoard.animate
+			properties:
+				y:902
+		EditBG2.animate
+			properties:
+				y:680
+		BG2.animate
+			properties:
+				y:680
+		imageGroup.animate
+			properties:
+				y:24
+		cursor.animate
+			properties:
+				opacity:1
+			curve:"bezier-curve(0.33,0,0.66,1)"
+			time:1
+			repeat:Infinity
+				
+	previewMode = ->
+		Utils.delay 0.6, ->
+			keyBoard.visible = false
+			
+		EditBG2.visible = false
+		cursor.opacity = 0
+		Edit.opacity = 1
+		Preview.opacity = 0
+		
+		keyBoard.animate
+			properties:
+				y:1334
+		EditBG2.animate
+			properties:
+				y:798
+		BG2.animate
+			properties:
+				y:798
+		imageGroup.animate
+			properties:
+				y:126
+		cursor.animate
+			properties:
+				opacity:0
+
+	publishMode = ->
+		Utils.delay 0.2, ->
+			step = 2
+		
+		PublishView.animate
+			properties:
+				y:41.2
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		newScrollView.animate
+			properties:
+				scale:0.24
+				x:29
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		BlackMask2.animate
+			properties:
+				opacity:0.5
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		Publish.animate
+			properties:
+				opacity:0
+		Publish2.animate
+			properties:
+				opacity:1
+		Back.animate
+			properties:
+				opacity:0
+		Back2.animate
+			properties:
+				opacity:1	
+		scroll.scrollVertical = false
+		if (Edit.opacity == 1)
+			Edit.animate
+				properties:
+					opacity:0
+		if (Preview.opacity == 1)
+			Preview.animate
+				properties:
+					opacity:0
+
+	republishMode = ->
+		step = 1
+		PublishView.animate
+			properties:
+				y:427
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		newScrollView.animate
+			properties:
+				scale:0.32
+				x:0
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		BlackMask2.animate
+			properties:
+				opacity:0
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.7
+		Publish.animate
+			properties:
+				opacity:1
+		Publish2.animate
+			properties:
+				opacity:0
+		Back.animate
+			properties:
+				opacity:1
+		Back2.animate
+			properties:
+				opacity:0	
+		scroll.scrollVertical = false
+		if (EditBG2.visible == true)
+			Preview.animate
+				properties:
+					opacity:1
+		else
+			Edit.animate
+				properties:
+					opacity:1
+
+	sendMode = ->
+		newGroup.animate
+			properties: 
+				y:-1500
+				shadowY: 10
+				shadowBlur: 10
+				shadowColor: "rgba(0,0,0,0.2)"
+			curve:"bezier-curve(0.55,0,0.1,1)"
+			time:0.6
+		newGroup.animate
+			properties: 
+				scale: 0.5
+			time:0.1
+			
+		Utils.delay 0.1, ->
+			newGroup.animate
+				properties: 
+					scale: 1
+				curve:"bezier-curve(0.55,0,0.1,1)"
+				time:0.6
+		
+		BG1.animate
+			properties:
+				scale:1
+		BlackMask.animate
+			properties:
+				opacity:0
+				
+		Utils.delay 0.6, ->
+			newGroup.destroy()
+			
+		Utils.delay 0.3, ->
+			newCard.animate
+				properties:
+					y:724
+					opacity:1
+					scale:1
+		
+		Utils.delay 2, ->
+			Center.destroy()
+			Rebirth()
+		
+		
+
+	# Publish Action
+	Publish2.on Events.Click, ->
+		if (step == 1)
+			publishMode()
+		else
+			sendMode()
+		
+
+		
+		
+	# Open & Close Action
+	newHitArea.on Events.Click, -> 
+		if (step == 1)
+			if (newHitArea.scale == 1)
+				openTheCard()
+			else 
+				closeTheCard()
+				cardOne.backgroundColor = "#fff"
+		if (step == 2)
+			republishMode()
+
+	EditHitArea.on Events.Click, ->
+		if (Edit.opacity == 1)
+			editMode()
+		else
+			previewMode()
+				
+	# OnScroll
+	scroll.on Events.Move, ->
+			scrolltoY(scroll.scrollY)
+			
+	scrolltoY = (y) ->
+			if (y>0)
+				navBar.opacity = Utils.modulate(y, [0, 20], [1, 0], true)
+				navBar.y = Utils.modulate(y, [0, 50], [0, -50], true)
+			else if (y>-60)
+				newGroup.y = Utils.modulate(y, [0, -400], [0, 800], true)
+				statusBar.y = Utils.modulate(y, [0, -400], [0, 400], true)
+				navBar.y = Utils.modulate(y, [0, -400], [0, 400], true)
+				BG1.scale = Utils.modulate(y, [0, -200], [0.8, 0.9], true)
+				BlackMask.opacity = Utils.modulate(y, [0, -200], [0.5, 0.1], true)
+			else
+				newGroup.y = Utils.modulate(y, [0, -400], [0, 800], true)
+				statusBar.y = Utils.modulate(y, [0, -400], [0, 400], true)
+				navBar.y = Utils.modulate(y, [0, -400], [0, 400], true)
+				BG1.scale = Utils.modulate(y, [0, -200], [0.8, 0.9], true)
+				BlackMask.opacity = Utils.modulate(y, [0, -200], [0.5, 0.1], true)
+				cardOne.backgroundColor = "clear"
+				closeTheCard()
+				
+Rebirth()
---
+++ CustomTransition.framer/imported/CustomTransition@2x/layers.json.js
@@ -0,0 +1,802 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["CustomTransition@2x/layers.json.js"] = [
+	{
+		"objectId": "73A81C5B-3E81-4123-AFD0-D7B57FE91777",
+		"kind": "artboard",
+		"name": "Center",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 983,
+			"y": 258,
+			"width": 375,
+			"height": 1564
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(239, 239, 244, 1)",
+		"children": [
+			{
+				"objectId": "80A3A9C5-918C-4ED1-9952-16BEC50E96C6",
+				"kind": "group",
+				"name": "PublishView",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 64,
+					"width": 375,
+					"height": 604
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-PublishView-odbbm0e5.png",
+					"frame": {
+						"x": 0,
+						"y": 64,
+						"width": 375,
+						"height": 604
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "7478DDAC-1B59-4A4C-B9DF-560E52CE4390",
+				"kind": "group",
+				"name": "BlackMask2",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 667
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-BlackMask2-nzq3oere.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 667
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "BD94A099-CDAC-43C6-AFE7-71A516D8F9CF",
+				"kind": "group",
+				"name": "newScrollView",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 1564
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-newScrollView-qkq5neew.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 1564
+					}
+				},
+				"children": [
+					{
+						"objectId": "AC4FADE9-7AF8-400A-89E6-11D782FFDE4E",
+						"kind": "group",
+						"name": "Mask_for_Text",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 400,
+							"width": 375,
+							"height": 1164
+						},
+						"visible": false,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Mask_for_Text-qum0rkfe.png",
+							"frame": {
+								"x": 0,
+								"y": 400,
+								"width": 375,
+								"height": 1164
+							}
+						},
+						"children": [
+							{
+								"objectId": "CCCF85E4-909B-4F56-B83A-383126DF3E58",
+								"kind": "group",
+								"name": "ShingingShape",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": -17,
+									"y": 383,
+									"width": 135,
+									"height": 315
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 0.6
+								},
+								"image": {
+									"path": "images/Layer-ShingingShape-q0ndrjg1.png",
+									"frame": {
+										"x": -17,
+										"y": 383,
+										"width": 135,
+										"height": 315
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "93496B86-5218-414D-8EBE-6DE7DC232361",
+								"kind": "group",
+								"name": "ShiningLayer",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 50,
+									"y": 450,
+									"width": 275,
+									"height": 180
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-ShiningLayer-otm0otzc.png",
+									"frame": {
+										"x": 50,
+										"y": 450,
+										"width": 275,
+										"height": 180
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "287D5308-EF7F-44EA-8A56-EFD12139143C",
+						"kind": "group",
+						"name": "keyBoard",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 451,
+							"width": 375,
+							"height": 216
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-keyBoard-mjg3rduz.png",
+							"frame": {
+								"x": 0,
+								"y": 451,
+								"width": 375,
+								"height": 216
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "4B6CE52F-7B45-4077-9DE9-EF5B8EB43494",
+						"kind": "group",
+						"name": "EditBG2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 399,
+							"width": 375,
+							"height": 503
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-EditBG2-nei2q0u1.png",
+							"frame": {
+								"x": 0,
+								"y": 399,
+								"width": 375,
+								"height": 503
+							}
+						},
+						"children": [
+							{
+								"objectId": "E17A305F-F6EE-4DA8-8CB3-8CEB2131B1D0",
+								"kind": "group",
+								"name": "cursor",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 199,
+									"y": 464,
+									"width": 3,
+									"height": 34
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-cursor-rte3qtmw.png",
+									"frame": {
+										"x": 199,
+										"y": 464,
+										"width": 3,
+										"height": 34
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "F96D1AF1-BA4C-4F59-91DB-EA6B052CA831",
+						"kind": "group",
+						"name": "BG2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 400,
+							"width": 375,
+							"height": 1164
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-BG2-rjk2rdfb.jpg",
+							"frame": {
+								"x": 0,
+								"y": 400,
+								"width": 375,
+								"height": 1164
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "1B278FF9-15B8-492D-83AC-22ECB405A47C",
+						"kind": "group",
+						"name": "imageGroup",
+						"maskFrame": {
+							"x": 0,
+							"y": 0.4028623204712858,
+							"width": 375.00000000000006,
+							"height": 335.9999999999999
+						},
+						"layerFrame": {
+							"x": 0,
+							"y": 64,
+							"width": 375,
+							"height": 337
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-imageGroup-muiynzhg.png",
+							"frame": {
+								"x": 0,
+								"y": 64,
+								"width": 375,
+								"height": 337
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "3449764F-DAC9-4F4C-8148-A7A964AC48F1",
+				"kind": "group",
+				"name": "statusBar",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 20
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-statusBar-mzq0otc2.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 20
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "721A5301-26D1-4917-A6FA-2CB46DAC763A",
+				"kind": "group",
+				"name": "navBar",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 65
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-navBar-nzixqtuz.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 65
+					}
+				},
+				"children": [
+					{
+						"objectId": "B68C8214-9CA1-496C-9951-6754ECC82739",
+						"kind": "group",
+						"name": "Edit",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 277,
+							"y": 23,
+							"width": 40,
+							"height": 37
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Edit-qjy4qzgy.png",
+							"frame": {
+								"x": 277,
+								"y": 23,
+								"width": 40,
+								"height": 37
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "275392D7-8D3A-4ADE-99B2-807A54039589",
+						"kind": "group",
+						"name": "Preview",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 277,
+							"y": 23,
+							"width": 40,
+							"height": 37
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Preview-mjc1mzky.png",
+							"frame": {
+								"x": 277,
+								"y": 23,
+								"width": 40,
+								"height": 37
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "FD618D7C-30B2-46F4-822E-AEB018521729",
+						"kind": "text",
+						"name": "Publish2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 334,
+							"y": 34,
+							"width": 32,
+							"height": 16
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "发布",
+							"css": [
+								"/* 发布: */",
+								"font-family: PingFangSC-Regular;",
+								"font-size: 17px;",
+								"color: #0076FF;",
+								"letter-spacing: -0.41px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-Publish2-rkq2mthe.png",
+							"frame": {
+								"x": 334,
+								"y": 34,
+								"width": 32,
+								"height": 16
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "5E4C7A4A-752A-4350-B641-B17E305C4EBA",
+						"kind": "text",
+						"name": "Publish",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 334,
+							"y": 34,
+							"width": 33,
+							"height": 16
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "投稿",
+							"css": [
+								"/* 投稿: */",
+								"font-family: PingFangSC-Regular;",
+								"font-size: 17px;",
+								"color: #0076FF;",
+								"letter-spacing: -0.41px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-Publish-nuu0qzdb.png",
+							"frame": {
+								"x": 334,
+								"y": 34,
+								"width": 33,
+								"height": 16
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "429D3550-1A10-4EEF-8149-B02624DE18D5",
+						"kind": "group",
+						"name": "Back2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 8,
+							"y": 31,
+							"width": 51,
+							"height": 22
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Back2-ndi5rdm1.png",
+							"frame": {
+								"x": 8,
+								"y": 31,
+								"width": 51,
+								"height": 22
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "AB47D015-38E7-46E6-8F20-C5220FED3FB8",
+						"kind": "group",
+						"name": "Back",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 8,
+							"y": 31,
+							"width": 85,
+							"height": 22
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Back-qui0n0qw.png",
+							"frame": {
+								"x": 8,
+								"y": 31,
+								"width": 85,
+								"height": 22
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "1B54C20F-E84D-4786-AC7F-C332B5847DE7",
+				"kind": "group",
+				"name": "newCard",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 15,
+					"y": 362,
+					"width": 120,
+					"height": 170
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-newCard-mui1nemy.png",
+					"frame": {
+						"x": 15,
+						"y": 362,
+						"width": 120,
+						"height": 170
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "DAEF8085-DB1A-42F8-BA85-BD71D11A921E",
+				"kind": "group",
+				"name": "Card2",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 145,
+					"y": 362,
+					"width": 120,
+					"height": 170
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"children": [
+					{
+						"objectId": "4C2A595F-01A3-4968-B862-9ECA77D46713",
+						"kind": "text",
+						"name": "nowDate2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 153,
+							"y": 510,
+							"width": 24,
+							"height": 12
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "昨天",
+							"css": [
+								"/* 昨天: */",
+								"font-family: PingFangSC-Regular;",
+								"font-size: 12px;",
+								"color: #808080;",
+								"letter-spacing: 0.2px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-nowDate2-nemyqtu5.png",
+							"frame": {
+								"x": 153,
+								"y": 510,
+								"width": 24,
+								"height": 12
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "7237B207-3EA7-4898-9DAE-4BDD35C6A5EE",
+						"kind": "text",
+						"name": "Title2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 153,
+							"y": 373,
+							"width": 97,
+							"height": 31
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "我最喜爱的设计风格：🍓",
+							"css": [
+								"/* 我最喜爱的设计风格：🍓: */",
+								"font-family: PingFangSC-Medium;",
+								"font-size: 12px;",
+								"color: #404040;",
+								"letter-spacing: 0.2px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-Title2-nzizn0iy.png",
+							"frame": {
+								"x": 153,
+								"y": 373,
+								"width": 97,
+								"height": 31
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "DC81D6D6-67E0-4C86-987B-A3B3737E7685",
+						"kind": "group",
+						"name": "CardBase2",
+						"maskFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 120,
+							"height": 170
+						},
+						"layerFrame": {
+							"x": 145,
+							"y": 362,
+							"width": 120,
+							"height": 170
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-CardBase2-rem4muq2.png",
+							"frame": {
+								"x": 145,
+								"y": 362,
+								"width": 120,
+								"height": 170
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "2EB92CD7-A58C-43A8-BC77-8634D18BD105",
+				"kind": "text",
+				"name": "nowDate",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 23,
+					"y": 510,
+					"width": 24,
+					"height": 12
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1,
+					"string": "昨天",
+					"css": [
+						"/* 昨天: */",
+						"font-family: PingFangSC-Regular;",
+						"font-size: 12px;",
+						"color: #808080;",
+						"letter-spacing: 0.2px;"
+					]
+				},
+				"image": {
+					"path": "images/Layer-nowDate-mkvcotjd.png",
+					"frame": {
+						"x": 23,
+						"y": 510,
+						"width": 24,
+						"height": 12
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "31D6439B-A277-4C33-89EB-D2B8E435C84F",
+				"kind": "text",
+				"name": "Title",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 23,
+					"y": 473,
+					"width": 97,
+					"height": 29
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1,
+					"string": "购物狂欢之后的新品盛宴",
+					"css": [
+						"/* 购物狂欢之后的新品盛宴: */",
+						"font-family: PingFangSC-Medium;",
+						"font-size: 12px;",
+						"color: #404040;",
+						"letter-spacing: 0.2px;"
+					]
+				},
+				"image": {
+					"path": "images/Layer-Title-mzfenjqz.png",
+					"frame": {
+						"x": 23,
+						"y": 473,
+						"width": 97,
+						"height": 29
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "711BF669-A74E-4709-8E80-F42A5607E639",
+				"kind": "group",
+				"name": "BlackMask",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 667
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-BlackMask-nzexqky2.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 667
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "348E8335-A35B-48F2-AA6A-AAEAAB94A146",
+				"kind": "group",
+				"name": "BG1",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 667
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-BG1-mzq4rtgz.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 667
+					}
+				},
+				"children": []
+			}
+		]
+	}
+]
