2ef168542eb377e59d7cd9de40c667ff9946745e
Martin <qiuyinsen@gmail.com>
2016-05-09 18:50:46 +0800

    add video ad
    add video ad

---
+++ ad_Video.framer/app.coffee
@@ -0,0 +1,437 @@
+#1. Import file "Video Framer Slice" (sizes and positions are scaled 1:2)
+sketch = Framer.Importer.load("imported/Video Framer Slice@2x")
+
+#2. Make 'sketch.' as global variable
+Utils.globalLayers(sketch)
+
+#3. Init Setting
+adBoard.opacity = 1
+adBoard.x = 40
+adBoard.y = 40
+Mask_Board.addSubLayer(adBoard)
+Mask_Board.clip = true
+
+hitarea1 = new Layer
+	x: 85,y:979,width: 128,height: 36, borderRadius: 2,opacity: 0, backgroundColor: "rgba(123,123,123,1)"
+Video.addSubLayer(hitarea1)
+
+	#3.1font Insert
+Utils.insertCSS('@import url(https://fonts.googleapis.com/css?family=Montserrat:700);')
+
+	#3.2 Cam Setting	
+Cam_Video = new VideoLayer
+	width: 750
+	height: 422
+	video: "images/Cam Video.mp4"
+	
+timenumber = new Layer
+	x:42,y:3,height: 28,width: 60,backgroundColor: "clear"
+Time_Label.addSubLayer(timenumber)
+timenumber.html = "1:24"
+timenumber.style.fontSize = "24px"
+timenumber.style.fontFamily = "'Montserrat', sans-serif"
+Video_Area.addSubLayer(Cam_Video)
+
+	#3.3 Animation Properties Setting
+alldelaytime = 0
+bezier1 = "bezier-curve(.35,.95,.56,.98)"
+bezier2 = "bezier-curve(.33,0,.25,1)"
+spring1 = 'spring(100,10,10)'
+
+	#3.4 Progress Cycle Animation & End
+progressArray = [Progress1,Progress2,Progress3,Progress4]
+
+progressInit = ->	
+	for progresses in progressArray
+		progresses.originY = 1
+		progresses.scaleY = 0
+		progresses.animateStop()
+progressInit()
+	#3.4.1 cycle Animation Function
+reverseAnim = (mylayer,delaynumber) ->
+	animationA = new Animation({
+		layer: mylayer,
+		properties: {scaleY: 0.2},
+		curve: "linear",
+		time: 0.3,
+		delay: 0.05*delaynumber
+	})
+	animationB = new Animation({
+		layer: mylayer,
+		properties: {scaleY: 1.0},
+		curve: "linear",
+		time: 0.3,
+		delay: 0.05*delaynumber
+	})
+	animationA.on(Events.AnimationEnd, animationB.start)
+	animationB.on(Events.AnimationEnd, animationA.start)
+	animationA.start()
+
+progressBegin = ->
+		
+	for i in [1...5]
+		myprogresses = sketch["Progress" + i]
+		reverseAnim(myprogresses,i)
+	
+progressEnd = ->
+	Progress1.animateStop()
+	Progress2.animateStop()
+	Progress3.animateStop()
+	Progress4.animateStop()
+	Utils.delay 0.2, ->
+		Progress1.animate
+			properties:
+				scaleY:0.2
+			curve: "linear"
+			time: 0.3
+		Progress2.animate
+			properties:
+				scaleY:0.4
+			curve: "linear"
+			time: 0.3
+		Progress3.animate
+			properties:
+				scaleY:0.6
+			curve: "linear"
+			time: 0.3
+		Progress4.animate
+			properties:
+				scaleY:0.8
+			curve: "linear"
+			time: 0.3
+	
+	
+	#3.5 CountDown Function
+countDownbeigin = ->
+	#3.5.1 Play Video
+	Cam_Video.player.play()
+	
+	#3.5.2 Cycle Animation
+	progressBegin()
+		
+	#3.5.3 Time Label
+	Utils.interval 1, ->
+		if Cam_Video.player.currentTime <= 14
+			timenumber.x = 42
+			timenumber.html = "1:"+"#{24-Math.round(Cam_Video.player.currentTime)}"
+		else if(Cam_Video.player.currentTime > 14 && Cam_Video.player.currentTime <= 24)
+			timenumber.x = 42
+			timenumber.html = "1:0"+"#{24-Math.round(Cam_Video.player.currentTime)}"
+		else if(Cam_Video.player.currentTime > 24 && Cam_Video.player.currentTime <= 74)
+			timenumber.x = 36
+			timenumber.html = "0:"+"#{60-(Math.round(Cam_Video.player.currentTime-24))}"
+		else
+			timenumber.x = 36
+			timenumber.html = "0:0"+"#{60-(Math.round(Cam_Video.player.currentTime-24))}"
+
+#4. State Machine Setting
+
+	#4.1 animateIn Group
+Tittle.states.add
+	closed:
+		y:504
+		opacity: 0
+	opened:
+		y:464
+		opacity: 1
+Tittle.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.25 + alldelaytime
+Tittle.states.switchInstant("closed")
+
+Detail_button.states.add
+	closed:
+		opacity: 0
+	opened:
+		opacity: 1
+Detail_button.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.25 + alldelaytime
+Detail_button.states.switchInstant("closed")
+
+describe.states.add
+	closed:
+		y:560
+		opacity: 0
+	opened:
+		y:510
+		opacity: 1
+describe.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.28 + alldelaytime
+describe.states.switchInstant("closed")
+
+	#4.2 baseIn Group
+Spinner.states.add
+	closed:
+		scale:0
+		opacity:0
+		y:184
+		rotation:0
+	opened:
+		scale:1
+		opacity:1
+		y:184
+	fadeout:
+		scale:0.6
+		opacity:0
+		y:165
+Spinner.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.3 + alldelaytime
+Spinner.states.switchInstant("closed")
+
+Star.states.add
+	closed:
+		x:78
+		y:148
+		scale:0.6
+		opacity:0
+	opened:
+		x:78
+		y:148
+		scale:1
+		opacity:1
+	fadeout:
+		x:78
+		y:128
+		scale:1
+		opacity:0
+Star.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.25 + alldelaytime
+Star.states.switchInstant("closed")
+
+Kuang.states.add
+	closed:
+		scale:1
+		y:-65
+		opacity: 0
+	opened:
+		scale:1
+		y:-25
+		opacity: 1
+	fadeout:
+		scale:1
+		y:-45
+		opacity:0
+Kuang.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.2 + alldelaytime
+Kuang.states.switchInstant("closed")
+
+Mask_Board.states.add
+	closed:
+		height:42
+	opened:
+		height:684
+Mask_Board.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0 + alldelaytime
+Mask_Board.states.switchInstant("closed")
+
+collapsedBtn.states.add
+	closed:
+		y:-32
+	opened:
+		y:277*2
+collapsedBtn.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0 + alldelaytime
+collapsedBtn.states.switchInstant("closed")
+
+hitarea1.states.add
+	closed:
+		opacity: 0
+	opened:
+		opacity: 0.3
+hitarea1.states.animationOptions =
+    curve: spring1
+hitarea1.states.switchInstant("closed")
+
+	#4.3 fadeIn Group
+Video_Area.states.add
+	closed:
+		opacity:0
+		scale:1
+	fadeout:
+		opacity:0
+		scale:0.8
+	fadein:
+		opacity:1
+		scale:1
+Video_Area.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.25 + alldelaytime
+Video_Area.states.switchInstant("fadeout")
+
+Play_Button.states.add
+	closed:
+		opacity:0
+		scale:1
+	fadeout:
+		opacity:0
+		scale:0.8
+	fadein:
+		opacity:1
+		scale:1
+Play_Button.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.3 + alldelaytime
+Play_Button.states.switchInstant("fadeout")
+
+Time_Label.states.add
+	closed:
+		opacity:0
+		scale:1
+	fadeout:
+		opacity:0
+		scale:0.8
+	fadein:
+		opacity:1
+		scale:1
+Time_Label.states.animationOptions =
+    curve: bezier1
+    time:0.5
+    delay:0.3 + alldelaytime
+Time_Label.states.switchInstant("fadeout")
+
+#5. Array Setting
+animationInArray = [hitarea1,collapsedBtn,Mask_Board,Kuang,Star,describe,Tittle,Detail_button,Spinner]
+fadeOutArray = [Spinner,Kuang,Star]
+fadeInArray = [Play_Button,Video_Area,Time_Label]
+
+#6. Animation Setting
+hitarea1.onClick ->
+	
+	for animLayers in animationInArray
+		animLayers.states.switch("opened")
+	
+	#6.1 Spinner Rotate
+	Spinner.animate
+		properties:
+			rotation:720
+		curve:"linear"
+		time:1.5
+		repeat:Infinity
+		
+	#6.2 Star Scale Anim
+	Utils.interval 0.7, ->
+		Star.animate
+			properties:
+				x: Utils.randomNumber 68, 88
+				y: Utils.randomNumber 138, 158
+				scale: Utils.randomNumber 0.95, 1.05
+			curve:"bezier-curve(0,0.33,0.66,1)"
+			time:1
+			
+	#6.3 Video FadeIn (Delay 3s)
+	Utils.delay 3,->
+		for fadeoutLayers in fadeOutArray
+			fadeoutLayers.states.switch("fadeout")
+		Utils.delay 0.25,->
+			for fadeinLayers in fadeInArray
+				fadeinLayers.states.switch("fadein")
+		Utils.delay 1,->
+			countDownbeigin()
+			Play_Button.states.switch("fadeout")
+
+Video_Area.onClick ->
+	if Cam_Video.player.onplaying = true
+		Play_Button.states.switch("fadein")
+		Cam_Video.player.pause()
+		progressEnd()
+		
+Play_Button.onClick ->
+	Play_Button.states.switch("fadeout")
+	Cam_Video.player.play()
+	countDownbeigin()
+
+collapsedBtn.onClick ->
+	# Must be clean instantly
+	Star.animateStop()
+	Spinner.animateStop()
+	Cam_Video.player.pause()
+	
+	for animLayers in animationInArray
+		animLayers.states.switch("closed")
+	
+	# Clean ,back to init
+	Utils.delay 0.5,->
+		for animLayers2 in fadeInArray
+			animLayers2.states.switchInstant("fadeout")
+		Cam_Video.player.currentTime = 0
+		progressInit()
+
+########################################Transition Part########################################
+
+blackMask = new Layer
+	x: 0,y:0,width: 750,height: 1334, borderRadius: 0,opacity: 0, backgroundColor: "black"
+
+blackMask.states.add
+	opened:
+		opacity: 0.5
+	closed:
+		opacity: 0
+blackMask.states.animationOptions =
+    curve: bezier2
+    time:0.5
+blackMask.states.switchInstant("closed")
+
+Video.states.add
+	opened:
+		x:-250
+		scale:0.9
+	closed:
+		x:0
+		scale:1
+Video.states.animationOptions =
+    curve: bezier2
+    time:0.5
+Video.states.switchInstant("closed")
+
+Page2 = new Layer
+	width: 750
+	height: 1334
+	image: "images/Page2.png"
+Page2.originX = 0
+Page2.originY = 0
+
+Page2.states.add
+	opened:
+		x:0
+	closed:
+		x:750
+Page2.states.animationOptions =
+    curve: bezier2
+    time:0.5
+Page2.states.switchInstant("closed")
+
+transitonArray = [blackMask,Video,Page2]
+
+Detail_button.onClick ->
+	Detail_button.backgroundColor = "rgba(216, 216, 216, 0.5)"
+	Utils.delay 0.1,->
+		Detail_button.backgroundColor = "clear"
+		for transitionLayers in transitonArray
+			transitionLayers.states.switch("opened")
+		
+		#Player Fix
+		Play_Button.states.switch("fadein")
+		Cam_Video.player.pause()
+		progressEnd()
+
+Page2.onClick ->
+	for transitionLayers in transitonArray
+			transitionLayers.states.switch("closed")
+
---
+++ ad_Video.framer/imported/Video Framer Slice@2x/layers.json.js
@@ -0,0 +1,509 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["Video Framer Slice@2x/layers.json.js"] = [
+	{
+		"objectId": "FF8AD4AA-2916-408A-B02D-79D67A52B013",
+		"kind": "artboard",
+		"name": "Video",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 129,
+			"y": 704,
+			"width": 375,
+			"height": 667
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"children": [
+			{
+				"objectId": "C8C1AEAD-7635-4DE7-A0E8-761EDD7482F5",
+				"kind": "group",
+				"name": "Top",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 65
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Top-qzhdmuff.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 65
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "9A2EAD80-8681-444A-9642-34B40FB1D8CE",
+				"kind": "group",
+				"name": "adBoard",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 64,
+					"width": 375,
+					"height": 286
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"children": [
+					{
+						"objectId": "1A8FDF3E-354A-4A47-A24E-6669664E64D5",
+						"kind": "group",
+						"name": "Spinner",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 162,
+							"y": 145,
+							"width": 51,
+							"height": 51
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Spinner-mue4rkrg.png",
+							"frame": {
+								"x": 162,
+								"y": 145,
+								"width": 51,
+								"height": 51
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "E2AFF5A9-EF5E-4D26-A416-EAC3411DB199",
+						"kind": "group",
+						"name": "Play_Button",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 158,
+							"y": 141,
+							"width": 60,
+							"height": 60
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Play_Button-rtjbrky1.png",
+							"frame": {
+								"x": 158,
+								"y": 141,
+								"width": 60,
+								"height": 60
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "D220CCD4-40D0-4CAE-BCAA-06B78DC9C595",
+						"kind": "text",
+						"name": "Tittle",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 28,
+							"y": 296,
+							"width": 70,
+							"height": 14
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "扫描全能王",
+							"css": [
+								"/* 扫描全能王: */",
+								"font-family: PingFangSC-Regular;",
+								"font-size: 14px;",
+								"color: #404040;",
+								"letter-spacing: 0px;",
+								"line-height: 14px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-Tittle-rdiymend.png",
+							"frame": {
+								"x": 28,
+								"y": 296,
+								"width": 70,
+								"height": 14
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "33E92BCB-5D64-4A38-AAD1-56F37F1C5164",
+						"kind": "text",
+						"name": "describe",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 28,
+							"y": 319,
+							"width": 103,
+							"height": 10
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1,
+							"string": "赞助商：Cam Scanner",
+							"css": [
+								"/* 赞助商：Cam Scanner: */",
+								"font-family: PingFangSC-Regular;",
+								"font-size: 10px;",
+								"color: #808080;",
+								"letter-spacing: 0px;",
+								"line-height: 10px;"
+							]
+						},
+						"image": {
+							"path": "images/Layer-describe-mznfotjc.png",
+							"frame": {
+								"x": 28,
+								"y": 319,
+								"width": 103,
+								"height": 10
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "9ED0F52F-EBFB-478B-9F13-7C7AB9BCEBDE",
+						"kind": "group",
+						"name": "collapsedBtn",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 177,
+							"y": 342,
+							"width": 21,
+							"height": 8
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-collapsedBtn-ouvemey1.png",
+							"frame": {
+								"x": 177,
+								"y": 342,
+								"width": 21,
+								"height": 8
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "3C4F7606-E8C7-4ECB-BA0E-9C13D28E0BA2",
+						"kind": "group",
+						"name": "Detail_button",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 288,
+							"y": 296,
+							"width": 60,
+							"height": 30
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Detail_button-m0m0rjc2.png",
+							"frame": {
+								"x": 288,
+								"y": 296,
+								"width": 60,
+								"height": 30
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "098247B1-A779-42BD-83ED-177BB0D51AF5",
+						"kind": "group",
+						"name": "Time_Label",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 298,
+							"y": 249,
+							"width": 49,
+							"height": 17
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Time_Label-mdk4mjq3.png",
+							"frame": {
+								"x": 298,
+								"y": 249,
+								"width": 49,
+								"height": 17
+							}
+						},
+						"children": [
+							{
+								"objectId": "218EE98D-D940-4881-9136-5A3BFE51BA34",
+								"kind": "group",
+								"name": "Progress4",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 311,
+									"y": 253,
+									"width": 2,
+									"height": 9
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Progress4-mje4ruu5.png",
+									"frame": {
+										"x": 311,
+										"y": 253,
+										"width": 2,
+										"height": 9
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "67351883-FCA2-42B9-9100-56912598A3DE",
+								"kind": "group",
+								"name": "Progress3",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 308,
+									"y": 253,
+									"width": 2,
+									"height": 9
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Progress3-njcznte4.png",
+									"frame": {
+										"x": 308,
+										"y": 253,
+										"width": 2,
+										"height": 9
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "9C0E857A-5191-4F20-BAB4-481B05A8BB59",
+								"kind": "group",
+								"name": "Progress2",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 305,
+									"y": 253,
+									"width": 2,
+									"height": 9
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Progress2-oumwrtg1.png",
+									"frame": {
+										"x": 305,
+										"y": 253,
+										"width": 2,
+										"height": 9
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "5C78D8AC-3732-47C7-84A9-46EC9F03D0FB",
+								"kind": "group",
+								"name": "Progress1",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 302,
+									"y": 253,
+									"width": 2,
+									"height": 9
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Progress1-num3oeq4.png",
+									"frame": {
+										"x": 302,
+										"y": 253,
+										"width": 2,
+										"height": 9
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "675110F4-C217-45EF-89AB-D9F9819FE992",
+						"kind": "group",
+						"name": "Video_Area",
+						"maskFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 375,
+							"height": 210.9375
+						},
+						"layerFrame": {
+							"x": 0,
+							"y": 65,
+							"width": 375,
+							"height": 211
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Video_Area-njc1mtew.png",
+							"frame": {
+								"x": 0,
+								"y": 65,
+								"width": 375,
+								"height": 211
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "DEDE2F71-A503-4B7F-953F-5278D57EBD29",
+						"kind": "group",
+						"name": "Star",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 39,
+							"y": 138,
+							"width": 297,
+							"height": 133
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Star-revertjg.png",
+							"frame": {
+								"x": 39,
+								"y": 138,
+								"width": 297,
+								"height": 133
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "7C05CEB7-B340-42BD-AF0A-77ACE29C7B3D",
+						"kind": "group",
+						"name": "Kuang",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 64,
+							"width": 375,
+							"height": 260
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-Kuang-n0mwnunf.png",
+							"frame": {
+								"x": 0,
+								"y": 64,
+								"width": 375,
+								"height": 260
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "C44429D1-6E94-4638-9AC6-349366E2FCA6",
+				"kind": "group",
+				"name": "Mask_Board",
+				"maskFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 302
+				},
+				"layerFrame": {
+					"x": -20,
+					"y": 44,
+					"width": 415,
+					"height": 342
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Mask_Board-qzq0ndi5.png",
+					"frame": {
+						"x": -20,
+						"y": 44,
+						"width": 415,
+						"height": 342
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "D20978F2-C8DE-4FC2-AB73-4B39DE2C408A",
+				"kind": "group",
+				"name": "Bottom",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 375,
+					"height": 667
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Bottom-rdiwotc4.jpg",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 375,
+						"height": 667
+					}
+				},
+				"children": []
+			}
+		]
+	}
+]
